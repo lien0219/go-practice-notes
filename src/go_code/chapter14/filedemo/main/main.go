ae99f1f5fe94b33d4fcf18b6961424026750c1aa
lien0219 <lien0001@163.com>
2024-06-23 22:50:49 +0800

    打开文件和关闭文件

---
+++ src/go_code/chapter14/filedemo/main/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	//	打开文件
+	//file 叫file对象 也可以叫 file指针 也可以叫 文件句柄
+	file, err := os.Open("D:\\桌面\\技术栈\\Golang\\go_project\\src\\go_code\\chapter14\\filedemo\\file\\test.txt")
+	if err != nil {
+		fmt.Println("open file err=", err)
+	}
+
+	fmt.Printf("file=%v", file) //file=&{0xc00009ca08}
+
+	//	关闭文件
+	err = file.Close()
+	if err != nil {
+		fmt.Println("close file err=", err)
+	}
+
+}
